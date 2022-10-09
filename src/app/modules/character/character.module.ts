04b9560f55ab8821f6e94b56fd56ec03508d6fdd
Voldemar.Bud <voldemar.bud@gmail.com>
2022-10-09 19:08:21 +0200

    try do

---
+++ src/app/modules/character/character.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharacterListComponent } from './character-list/character-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    CharacterListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CharacterModule { }
