078636ac31bed83c21a20423b3c40838e9624cc8
Voldemar.Bud <voldemar.bud@gmail.com>
2022-10-09 19:11:51 +0200

    import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CharacterModule } from './modules/character/character.module';
+import { LocationModule } from './modules/location/location.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CharacterModule,
+    LocationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
