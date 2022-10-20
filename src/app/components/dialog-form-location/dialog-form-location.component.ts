6ede3fb50112ebc190eab0b40c520861b75002c1
Voldemar.Bud <voldemar.bud@gmail.com>
2022-10-20 22:04:32 +0200

    use FormBuilder

--- src/app/components/dialog-form-location/dialog-form-location.component.ts
+++ src/app/components/dialog-form-location/dialog-form-location.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Locations } from 'src/app/modules/locations/store/modules/locations';
 
@@ -12,28 +12,49 @@ export class DialogFormLocationComponent {
   locationFormModel: FormGroup;
 
   constructor(
+    private dialogFormBuilder: FormBuilder,
     public dialogRef: MatDialogRef<DialogFormLocationComponent>) {
-    this.locationFormModel = new FormGroup({
-      url: new FormControl('', [
-        Validators.required,
-        Validators.minLength(2)]),
-      name: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4)]),
-      type: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3)]),
-      created: new FormControl('',
-        Validators.required)
-    })
+    this._createForm();
   }
+
+  private _createForm() {
+    this.locationFormModel = this.dialogFormBuilder.group({
+      url: ['', [
+        Validators.required,
+        Validators.minLength(2)
+      ]],
+      name: ['', [
+        Validators.required,
+        Validators.minLength(4)
+      ]],
+      type: ['', [
+        Validators.required,
+        Validators.minLength(3)
+      ]],
+      created: ['', [Validators.required]]
+    })
+       // this.locationFormModel = new FormGroup({
+    //   url: new FormControl('', [
+    //     Validators.required,
+    //     Validators.minLength(2)]),
+    //   name: new FormControl('', [
+    //     Validators.required,
+    //     Validators.minLength(4)]),
+    //   type: new FormControl('', [
+    //     Validators.required,
+    //     Validators.minLength(3)]),
+    //   created: new FormControl('',
+    //     Validators.required)
+    // })
+  }
+
   onSubmit() {
     console.log({
       ...this.locationFormModel.value,
       url: 'https://rickandmortyapi.com/api/location/' + this.locationFormModel.value.url
     })
 
-  
+
     // this.dialogRef.close(newlocation);
   }
   onCancel(): void {
