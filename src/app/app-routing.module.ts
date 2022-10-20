1665febd2b7f0a56a5fc41bf4c72a430b045414b
Voldemar <38044280+VoldemarBud@users.noreply.github.com>
2022-10-20 15:38:49 +0200

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CharacterDetailsComponent } from './modules/character/components/character-details/character-details.component';
-import { CharacterListComponent } from './modules/character/components/character-list/character-list.component';
 
 const routes: Routes = [
-
   {
-    path: 'character-list',
-    component: CharacterListComponent
+    path: ' ',
+    redirectTo: 'character-list'
   },
   {
-    path: 'character/:id',
-    component: CharacterDetailsComponent
-  },
-  {
-    path:'**',
+    path: '**',
     redirectTo: 'character-list'
   }
 ];
