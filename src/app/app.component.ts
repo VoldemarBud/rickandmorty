b04e6313aebd4b878748c0f5611212ab3b0439a8
Voldemar <38044280+VoldemarBud@users.noreply.github.com>
2022-10-20 15:49:59 +0200

    add character and location service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,22 @@
 
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { HttpReq } from './http/http';
 
-import { getCharacterList } from './modules/character/store/character.action';
-import { getLocationsList } from './modules/locations/store/locations.actions';
+import { CharacterService } from './modules/character/character.service';
+import { LocationsService } from './modules/locations/character.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [CharacterService, LocationsService]
 })
 export class AppComponent implements OnInit {
 
 
-  constructor(private store$: Store<{ charactersList: [] }>,
-    private httpReq: HttpReq) {
+  constructor(private characterService: CharacterService, private locationsService: LocationsService) {
   }
   ngOnInit(): void {
-    this.httpReq.getCharacterList().subscribe((res: any) => {
-      if (res?.results?.length) {
-        const { info, results } = res;
-        this.store$.dispatch(getCharacterList({ data: results }));
-      }
-
-      this.httpReq.getLocationsList().subscribe((res: any) => {
-        if (res?.results?.length) {
-          const { info, results } = res;
-          this.store$.dispatch(getLocationsList({ data: results }));
-        }
-      })
-    })
+    this.characterService.getList();
+    this.locationsService.getList();
   }
 }
