63be846e489d6d5900f13e18faa156010c059495
Voldemar.Bud <voldemar.bud@gmail.com>
2022-10-21 20:56:53 +0200

    try @ngrx/entity

--- src/app/modules/character/store/character.action.ts
+++ src/app/modules/character/store/character.action.ts
@@ -1,18 +1,32 @@
-import { createAction, props } from "@ngrx/store";
+import { Action, createAction, props } from "@ngrx/store";
 import { Character } from "./modules/characters";
 
-export const getCharacterList = createAction('[CHARACTER] get character list',
-    props<{ data: Character[] }>()
-);
+export enum CharactersActionTypes {
+    LoadCharacters = '[CHARACTERS] Load CharacterS',
+    DeleteCharacter = '[CHARACTERS] Delete CHARACTER',
+}
 
-export const getCharacter = createAction('[CHARACTER] get character',
-    props<{ id: number }>()
-);
+// createAction('[CHARACTER] get character list',
+// props<{ data: Character[] }>()
+// );
 
-export const setNewCharacterInfo = createAction('[CHARACTER] set new character info',
-    props<{ data: Character }>()
-);
+export class LoadCharacterList implements Action {
+    readonly type = CharactersActionTypes.LoadCharacters;
+    constructor(public payload: { charactersList: Character[] }) { }
+}
 
-export const deleteCharacter = createAction('[CHARACTER] delete character',
-    props<{ id: number }>()
-)
+export class DeleteCharacter implements Action {
+    readonly type = CharactersActionTypes.DeleteCharacter;
+
+    constructor(public payload: { id: string }) { }
+}
+// export const getCharacter = createAction('[CHARACTER] get character',
+//     props<{ id: number }>()
+// );
+
+// export const deleteCharacter = createAction('[CHARACTER] delete character',
+//     props<{ id: number }>()
+// )
+
+
+export type CharactersActions = LoadCharacterList | DeleteCharacter
--- src/app/modules/character/store/character.reducer.ts
+++ src/app/modules/character/store/character.reducer.ts
@@ -1,48 +1,68 @@
 
 import { Character } from "./modules/characters";
-import { deleteCharacter, getCharacter, getCharacterList, setNewCharacterInfo } from "./character.action";
-import { createReducer, on } from '@ngrx/store';
-
+import { createFeatureSelector, createReducer, on } from '@ngrx/store';
+import { createEntityAdapter, EntityState } from "@ngrx/entity";
+import { CharactersActions, CharactersActionTypes } from "./character.action";
 export const CHARACTER_REDUCER_NODE = 'characters';
 
 export interface CharactersState {
-    charactersList: Character[] | [],
-    character?: Character | {}
+    charactersList: Character[] | []
 }
 const initialCharactersState: CharactersState = {
     charactersList: []
 }
 
-export const charactersReducer = createReducer(
-    initialCharactersState,
-    on(getCharacterList, (state, { data }) => ({
-        ...state,
-        charactersList: data
-    })),
-    on(getCharacter, (state, { id }) => ({
-        ...state,
-        character: state.charactersList[id]
-    })),
+const defaultCharacters = {
+    ids: [],
+    entities: {
 
-    on(setNewCharacterInfo, (state, { data}) => {
-        return ({
-            ...state,
-            charactersList: state.charactersList.map((element, i) => {
-                if (element.id == data.id) {
-                    return element = data
-                }
-                return element
-            }),
-            character: data
-        })
-    }),
-    on(deleteCharacter, (state, { id }) => ({
-        ...state,
-        charactersList: state.charactersList.filter((element) => {
-            if (element.id != id) {
-                return true;
-            }
-            return false
-        }),
-    }))
-);
+
+    }
+}
+
+
+
+export const charactersAdapter = createEntityAdapter<Character>();
+
+export interface State extends EntityState<Character> {
+}
+export const initialCharactersStateE: State = charactersAdapter.getInitialState();
+
+
+// export const charactersReducer = createReducer(
+//     initialCharactersState,
+//     on(GetCharacterList, (state, { data }) => ({
+//         ...state,
+//         charactersList: data
+//     })),
+//     on(getCharacter, (state, { id }) => ({
+//         ...state,
+//         character: state.charactersList[id]
+//     })),
+
+//     on(deleteCharacter, (state, { id }) => ({
+//         ...state,
+//         charactersList: state.charactersList.filter((element) => {
+//             if (element.id != id) {
+//                 return true;
+//             }
+//             return false
+//         }),
+//     }))
+// );
+
+export function charactersReducer(
+    state: State = initialCharactersStateE,
+    action: CharactersActions
+) {
+    switch (action.type) {
+        case CharactersActionTypes.LoadCharacters:
+            return charactersAdapter.addMany(action.payload.charactersList, state);
+        case CharactersActionTypes.DeleteCharacter:
+            return charactersAdapter.removeOne(action.payload.id, state)
+        default:
+            return state
+    }
+}
+
+export const getCharacterState = createFeatureSelector<State>(CHARACTER_REDUCER_NODE);
