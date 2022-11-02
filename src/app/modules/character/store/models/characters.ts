3d404004d41f6bdf263d377017fc498e3b39507f
Voldemar <38044280+VoldemarBud@users.noreply.github.com>
2022-11-02 11:53:17 +0100

    Update characters.ts

--- src/app/modules/character/store/models/characters.ts
+++ src/app/modules/character/store/models/characters.ts
@@ -3,11 +3,18 @@ export interface Character {
     name: string,
     status: string,
     species: string,
+    type?: string,
     gender: string,
-    image: string,
-    location: {
+    origin?: {
+        name: string,
+        url: string
+    },
+     location: {
         name: string,
         url?: string
-    },
+    }, 
+    image: string,
+    episode?: [string],
+    url?: string,
     created: string
 }
