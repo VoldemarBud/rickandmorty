06978a2b661897d356c86111299a981bc9191bab
Voldemar <38044280+VoldemarBud@users.noreply.github.com>
2022-11-08 11:43:17 +0100

    Update locations-request.service.ts

--- src/app/modules/locations/store/locations-request.service.ts
+++ src/app/modules/locations/store/locations-request.service.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { LocationsModule } from '../locations.module';
-
 
 @Injectable({providedIn:'root'})
 export class LocationsRequestService {
